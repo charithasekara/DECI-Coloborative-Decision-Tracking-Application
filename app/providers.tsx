4b7a9950e53afe47fcf5c59a0a7e3c40be110d65
charithasekara <86426242+charithasekara@users.noreply.github.com>
2025-05-17 19:01:36 +0530

    Layout changes

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,12 @@
 
 import { ThemeProvider } from 'next-themes';
 import { AnimatePresence } from 'framer-motion';
+import React from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <AnimatePresence mode="wait">
+      <AnimatePresence mode="sync">
         {children}
       </AnimatePresence>
     </ThemeProvider>
